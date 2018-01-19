4a93e574df694d09f02d5926474860a4d7c6ab9b
Eric Bartos <djbartos93@gmail.com>
2018-01-19 12:09:45 -0600

    Logstash deploy
    added logstash to the deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,33 @@
 #!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "NOT ROOT! TRY AGAIN!"
+   exit 1
+fi
+
+
+cat << "EOF"
+             ((((
+            ((((
+             ))))
+          _ .---.
+         ( |`---'|
+          \|     |
+          : .___, :
+           `-----'
+Brewing your configs!
+EOF
+
+git pull --rebase
+
+read -p "Are you sure? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  CWD="$(pwd)"
+  LSD="/etc/logstash"
+  cd "$CWD"/Logstash
+  echo "copying logstash configs"
+  cp *.conf $LSD
+  echo "DONE! Please restart logstash!"
+fi
